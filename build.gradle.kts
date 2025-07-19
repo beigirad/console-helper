078385819d07fac9975f91655c11297778356030
Farhad Beigirad <f.beigirad@gmail.com>
2025-07-19 08:39:52 +0330

    feat: configure publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("jvm") version "2.1.0"
+    `maven-publish`
 }
+
 repositories {
     mavenCentral()
 }
@@ -14,4 +16,35 @@ dependencies {
 
 kotlin {
     jvmToolchain(17)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("jline-helper"){
+            from(project.components["kotlin"])
+            groupId = group.toString()
+            artifactId = "jline-helper"
+            version = version.toString()
+            pom {
+                name.set("jline-helper")
+                description.set("A helper library to work with console in kotlin-script ")
+                url.set("https://github.com/beigirad/jline-helper")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://github.com/beigirad/jline-helper/blob/main/LICENSE")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/beigirad/jline-helper")
+                }
+                developers {
+                    developer {
+                        name.set("Farhad Beigirad")
+                        url.set("https://github.com/beigirad")
+                    }
+                }
+            }
+        }
+    }
 }
