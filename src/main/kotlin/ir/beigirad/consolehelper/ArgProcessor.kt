526619bc23cd7e980939f9700e9cc90ac9c61054
Farhad Beigirad <f.beigirad@gmail.com>
2025-07-23 10:13:14 +0330

    fix: support multi-line argument for arg processor

--- src/main/kotlin/ir/beigirad/consolehelper/ArgProcessor.kt
+++ src/main/kotlin/ir/beigirad/consolehelper/ArgProcessor.kt
@@ -1,13 +1,16 @@
 package ir.beigirad.jlinehelper
 
 class ArgProcessor(rawArgs: Array<String>) {
-    private val regex = Regex("--(.*)=(.*)")
+    private val regex = Regex("--(.*)=(.*)", RegexOption.DOT_MATCHES_ALL)
     private val arg = rawArgs.mapNotNull {
         val matches = regex.find(it) ?: return@mapNotNull null
         matches.groupValues[1] to matches.groupValues[2]
     }.toMap()
 
     fun getOrNull(key: String): String? = arg[key]
+
     operator fun get(key: String): String =
         checkNotNull(getOrNull(key)) { "Required '$key' was null." }
+
+    override fun toString(): String = "Args(${arg})"
 }
