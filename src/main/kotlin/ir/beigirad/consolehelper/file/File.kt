60c3cb8f661696b1d58ac61d4efd065d2238a07b
Farhad Beigirad <f.beigirad@gmail.com>
2025-12-31 20:04:37 +0330

    feat: add rootDir function to find git root directory

--- src/main/kotlin/ir/beigirad/consolehelper/file/File.kt
+++ src/main/kotlin/ir/beigirad/consolehelper/file/File.kt
@@ -1,5 +1,8 @@
 package ir.beigirad.consolehelper.file
 
+import ir.beigirad.consolehelper.shell.runCommand
 import java.io.File
 
 operator fun File.div(next: String): File = File("${this.path}/$next")
+
+fun rootDir(): File = File("git rev-parse --show-toplevel".runCommand().trim())
