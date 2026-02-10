c3f91d6cbde9ca07cb6b7d7f794cd221b8881fac
Farhad Beigirad <f.beigirad@gmail.com>
2026-02-10 10:46:19 +0330

    feat: provide rootPath against rootDir

--- src/main/kotlin/ir/beigirad/consolehelper/file/File.kt
+++ src/main/kotlin/ir/beigirad/consolehelper/file/File.kt
@@ -2,7 +2,13 @@ package ir.beigirad.consolehelper.file
 
 import ir.beigirad.consolehelper.shell.runCommand
 import java.io.File
+import java.nio.file.Path
+import kotlin.io.path.Path
 
 operator fun File.div(next: String): File = File("${this.path}/$next")
 
-fun rootDir(): File = File("git rev-parse --show-toplevel".runCommand().trim())
+private fun rootOfGit(): String = "git rev-parse --show-toplevel".runCommand().trim()
+
+fun rootDir(): File = File(rootOfGit())
+
+fun rootPath(): Path = Path(rootOfGit())
